b8bf7fb3cea43319d8bb78ab6181d5c93ae6fb00
Nils Geschwinde <nils.geschwinde@inside-m2m.de>
2021-10-20 12:01:19 +0200

    added action constants and creators

--- src/app.ts
+++ src/app.ts
@@ -20,10 +20,7 @@ button.addEventListener(
 
 		const payload = { label: input.value, complete: false };
 
-		store.dispatch({
-			type: "ADD_TODO",
-			payload,
-		});
+		store.dispatch(new fromStore.AddTodo(payload));
 
 		input.value = "";
 	},
@@ -37,7 +34,8 @@ destroy.addEventListener("click", unsub, false);
 todoList.addEventListener("click", function (event) {
 	const target = event.target as HTMLButtonElement;
 	if (target.nodeName.toLowerCase() === "button") {
-		console.log(target);
+		const todo = JSON.parse(target.getAttribute("data-todo") as any);
+		store.dispatch(new fromStore.RemoveTodo(todo));
 	}
 });
 
